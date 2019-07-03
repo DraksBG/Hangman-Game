7b01e7c447420b81874a174a2e61660b73afbbb3
Dimitar Krastanov <DraksBG@users.noreply.github.com>
2019-07-03 15:39:49 +0300

    On branch master  Your branch is up to date with 'origin/master'.

--- Program.cs
+++ Program.cs
@@ -8,8 +8,45 @@ namespace Hangman_game
 {
     class Program
     {
-        static void Main(string[] args)
+        //static List<String> GeneratingRandomWords(List<string> words ,string word )
+        //{
+        //    return string;
+        //}
+
+        //static char GeneratingCharFromString(string word, char guessed)
+        //{
+        //    return string by char indexes so they could be guessed;
+        //}
+        //static char TransformingCharToInvisible(char guessed , char[] symbols)
+        //{
+        //    return char[] from the upper method to '_' so the user can't see the word 
+        //}
+        //static int Guesses(int input, int numbers, char[] guesses)
+        //{
+        //    return in the begging of the game there would be a 10 gusess possible with input form the user if the 
+        //        input dosen't match with the correct one form the word the guesses decreesed if he sucssefuly 
+        //        match the char it stays the same number of guesses
+        //}
+        //static bool Update (int input, int number, char guesses, char symbols)
+        //{
+        //    return with if statmen if true if the user guessed the right char from the word it apears or 
+        //        if false if it dosent it his guesses are decreesed
+        //}
+        //static bool GameRunningUntil(int input, int number, string word, char guessed)
+        //{
+        //    return the game will run while the user sucessfuly input all the char until the guesses hasn't ended
+        //        or if the guesses becomes 0;
+        //}
+
+    static void Main(string[] args)
         {
+            //string Remeber = GeneratingRandomWords;
+
+            //while (true)
+            //{
+            //    //here will go all the other methods
+            //}
+
         }
     }
 }
